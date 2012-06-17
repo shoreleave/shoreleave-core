4317a0d4e13aa6b89b5d4d0935f7512f540f6067
Paul deGrandis <paul.degrandis@gmail.com>
2012-06-17 17:45:53 -0400

    Documentation

--- src/shoreleave/brepl.cljs
+++ src/shoreleave/brepl.cljs
@@ -1,6 +1,8 @@
 (ns shoreleave.brepl
   (:require [clojure.browser.repl :as repl]))
 
-(defn connect []
+(defn connect
+  "Shorthand for the CLJS browser repl connect call"
+  []
   (repl/connect "http://localhost:9000/repl"))
 
--- src/shoreleave/efunction.cljs
+++ src/shoreleave/efunction.cljs
@@ -1,4 +1,16 @@
-(ns shoreleave.efunction)
+(ns shoreleave.efunction
+  "A Function object abstraction")
+
+;; The Function type is used as a decorator base for functions.
+;;
+;; This comes in handy if you want create specialty function types
+;; (like embedded workers) or want to extend or change the behavior
+;; of the base function type.
+;;
+;; In the [pubsub system](https://github.com/shoreleave/shoreleave-pubsub),
+;; the Function type is used to decorate functions that can be published to.
+;;
+;; These enhanced function types are hashable and support metadata
 
 (deftype Function [f meta]
   
