6782ed8ea47c77b3e4d330a4a954eebeda13c80b
Paul deGrandis <paul.degrandis@gmail.com>
2012-06-18 15:20:47 -0400

    project updates

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject shoreleave-core "0.2.1"
+(defproject shoreleave/shoreleave-core "0.2.1"
   :description "A smarter client-side with ClojureScript : Shoreleave's core auxiliary functions"
   :url "http://github.com/shoreleave"
   :license {:name "Eclipse Public License - v 1.0"
@@ -6,10 +6,8 @@
             :distribution :repo
             :comments "See the notice in README.mkd or details in LICENSE_epl.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :dev-dependencies [[vimclojure/server "2.3.3" :exclusions [org.clojure/clojure]] 
-                     ;[cdt "1.2.6.2-SNAPSHOT"]
+  :dev-dependencies [;[cdt "1.2.6.2-SNAPSHOT"]
                      ;[lein-cdt "1.0.0"] ; use lein cdt to attach
                      ;[lein-autodoc "0.9.0"]
-                     [lein-marginalia "0.7.1"]]
-  :plugins  [[lein-cljsbuild "0.2.1"]])
+                     [lein-marginalia "0.7.1"]])
 
